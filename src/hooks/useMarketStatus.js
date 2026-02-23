eda183be468b80d210a0ceb2787be8862600f381
Manoj Kumar <bmk7829@gmail.com>
2026-02-23 19:34:12 +0530

    Update frontend to show Market Closed

--- src/hooks/useMarketStatus.js
+++ src/hooks/useMarketStatus.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
 export function useMarketStatus() {
-    // Return true statically so you can showcase the app even when the market is closed.
-    return true;
+    // Return false statically as requested by the user
+    return false;
 }
